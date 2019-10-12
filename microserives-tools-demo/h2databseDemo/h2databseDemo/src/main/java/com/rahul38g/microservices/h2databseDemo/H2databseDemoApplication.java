f044b1d95b28b31942c2b7606172cf186a2e8073
Rahul <36669594+rahul38g-gmail@users.noreply.github.com>
2019-10-13 01:21:51 +0530

    first commit

---
+++ microserives-tools-demo/h2databseDemo/h2databseDemo/src/main/java/com/rahul38g/microservices/h2databseDemo/H2databseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rahul38g.microservices.h2databseDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2databseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2databseDemoApplication.class, args);
+	}
+
+}
---
+++ microserives-tools-demo/h2databseDemo/h2databseDemo/src/test/java/com/rahul38g/microservices/h2databseDemo/H2databseDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rahul38g.microservices.h2databseDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class H2databseDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
